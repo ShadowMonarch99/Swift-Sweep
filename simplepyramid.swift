eaaff12146616668fc7f75a90d6cb984918f1de8
Apoorv Joshi <52959228+ShadowMonarch99@users.noreply.github.com>
2021-12-22 01:04:02 +0530

    Add files via upload

---
+++ simplepyramid.swift
@@ -0,0 +1,21 @@
+/**
+
+                            Online Swift Compiler.
+                Code, Compile, Run and Debug Swift script online.
+Write your code in this editor and press "Run" button to execute it.
+
+*/
+
+
+import Foundation
+var n=Int(readLine()!)
+for i in 0...n!{
+    for j in 0...i{
+        print("*",terminator:"")
+    }
+    print()
+    
+}
+
+
+
