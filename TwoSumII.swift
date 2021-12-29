14a5a8a9bfca46177a7e5b36b4398fac30c51cfd
Apoorv Joshi <52959228+ShadowMonarch99@users.noreply.github.com>
2021-12-29 23:56:56 +0530

    Create TwoSumII.swift

---
+++ TwoSumII.swift
@@ -0,0 +1,28 @@
+//Leetcode
+//Given a 1-indexed array of integers numbers that is already sorted in non-decreasing order, find two numbers such that they add up to a specific target number. Let these two numbers be numbers[index1] and numbers[index2] where 1 <= index1 < index2 <= numbers.length.
+
+//Return the indices of the two numbers, index1 and index2, added by one as an integer array [index1, index2] of length 2.
+
+//The tests are generated such that there is exactly one solution. You may not use the same element twice.
+
+class Solution {
+    func twoSum(_ numbers: [Int], _ target: Int) -> [Int] {
+        if numbers.count <= 1 {
+            return [Int]()
+        }
+        
+        var left = 0, right = numbers.count - 1
+        
+        while left < right {
+            if numbers[left] + numbers[right] < target {
+                left += 1
+            } else if numbers[left] + numbers[right] > target {
+                right -= 1
+            } else {
+                return [left + 1, right + 1]
+            }
+        }
+        
+        return [Int]()
+    }
+}
